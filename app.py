96851eea451761cde9e4b7da4142a1b95fc7ac20
BARATH KUMAR <82716580+Barath5647@users.noreply.github.com>
2025-06-18 11:24:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ IMG_SIZE = (64, 64)  # This should match the image size used during training
 model = load_model(
     'cnn_model.h5',
     custom_objects={'DTypePolicy': Policy}
+    compile=False
 )
 
 
